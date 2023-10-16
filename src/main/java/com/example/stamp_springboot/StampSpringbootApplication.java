85f907faf57dc8dad7f024b98f13c1a531c77c4f
minyoung555 <mindud02@naver.com>
2023-10-16 19:43:15 +0900

    init

---
+++ src/main/java/com/example/stamp_springboot/StampSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.example.stamp_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StampSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StampSpringbootApplication.class, args);
+	}
+
+}
