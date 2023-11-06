cc18f9dcc02982693144478b67f0a8b586bde829
minyoung555 <mindud02@naver.com>
2023-11-06 16:37:52 +0900

    modify ShopModel.java

--- src/main/java/com/example/stamp_springboot/model/ShopModel.java
+++ src/main/java/com/example/stamp_springboot/model/ShopModel.java
@@ -4,8 +4,13 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.Date;
 
 @NoArgsConstructor
 @AllArgsConstructor
@@ -14,5 +19,16 @@ import org.springframework.data.mongodb.core.mapping.Document;
 @Document(collection="shop")
 public class ShopModel {
     @Id
-    private String id;
+    private String shop_id;
+
+    private String shop_name;
+
+    @Indexed(unique = true)
+    private String business_number;
+
+    @CreatedDate
+    private Date createTime;
+
+    @LastModifiedDate
+    private Date updateTime;
 }
