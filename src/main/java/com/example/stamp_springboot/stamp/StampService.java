1dad2f85b7510de4ee2f927c58843a1a93fcf239
ParkWonYeop <pwy9679@gnu.ac.kr>
2023-11-08 16:36:44 +0900

    stamp

---
+++ src/main/java/com/example/stamp_springboot/stamp/StampService.java
@@ -0,0 +1,11 @@
+package com.example.stamp_springboot.stamp;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
+public class StampService {
+}
