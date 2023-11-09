7db640e5226ac5e0ae0184147390795a9f84fe81
minyoung555 <mindud02@naver.com>
2023-11-09 09:21:11 +0900

    가게 이름 수정 기능 추가 StampSpringbootApplication에 @EnableMongoAuditing 추가 자잘한 구조 수정

--- src/main/java/com/example/stamp_springboot/shop/ShopService.java
+++ src/main/java/com/example/stamp_springboot/shop/ShopService.java
@@ -7,9 +7,9 @@ import com.example.stamp_springboot.model.ShopModel;
 import com.example.stamp_springboot.repository.ShopRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.stereotype.Service;
 
-import java.util.NoSuchElementException;
 import java.util.Optional;
 
 @Slf4j
@@ -25,32 +25,36 @@ public class ShopService {
     }
 
     // 가게 등록
-    public void signup(ShopSignupDto shopSignupDto) {
+    public String signup(ShopSignupDto shopSignupDto) {
         String business_number = shopSignupDto.getBusinessNumber();
 
         Optional<ShopModel> existingShop = shopRepository.findByBusinessNumber(business_number);
 
         if (existingShop.isPresent()) {
             log.error("이미 존재하는 가게");
+            return "이미 존재하는 가게입니다.";
         }
         else {
             ShopModel shopModel = shopMapper.signDtoToShopModel(shopSignupDto);
             shopRepository.save(shopModel);
             log.info("가게 등록 성공");
+            return "가게가 등록되었습니다.";
         }
     }
 
     // 가게 로그인
-    public void login(ShopLoginDto shopLoginDto) {
+    public String login(ShopLoginDto shopLoginDto) {
         String business_number = shopLoginDto.getBusinessNumber();
 
         Optional<ShopModel> existingShop = shopRepository.findByBusinessNumber(business_number);
 
         if (existingShop.isPresent()) {
             log.info("로그인");
+            return "로그인되었습니다.";
         }
         else {
             log.error("존재하지 않는 가게");
+            return "존재하지 않는 가게입니다.";
         }
     }
 
@@ -64,6 +68,30 @@ public class ShopService {
         else {
             log.info("존재하지 않는 가게");
             return Optional.empty();
+        }
+    }
+
+    // 가게 이름 수정
+    public String updateShopName(String businessNumber, String newName) {
+        Optional<ShopModel> existingshop = shopRepository.findByBusinessNumber(businessNumber);
+
+        if (existingshop.isPresent()) {
+            String shopName = existingshop.get().getShop_name();
+
+            if (newName.equals(shopName)) {
+                log.info("수정 전 이름, 수정 후 이름 같음");
+                return "수정 전 이름과 수정 후 이름이 같습니다.";
+            } else {
+                ShopModel shopModel = existingshop.get();
+                shopModel.setShop_name(newName);
+                shopRepository.save(shopModel);
+                log.info("수정 완료");
+                return "수정되었습니다.";
+            }
+        }
+        else {
+            log.error("존재하지 않는 가게");
+            return "존재하지 않는 가게입니다.";
         }
     }
 }
