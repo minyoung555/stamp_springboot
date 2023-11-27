c23113010471347eeec7bd453c4a7e6b79a6dce9
ParkWonYeop <pwy9679@gnu.ac.kr>
2023-11-27 16:32:20 +0900

    Stamp

--- src/main/java/com/example/stamp_springboot/stamp/StampService.java
+++ src/main/java/com/example/stamp_springboot/stamp/StampService.java
@@ -11,10 +11,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
 import java.util.concurrent.ExecutionException;
 
 @Service
@@ -32,17 +29,20 @@ public class StampService {
                 if(userModel.isPresent()) {
                     List<StampModel> stampModelList = userModel.get().getStamps();
                     for (StampModel stampModel : stampModelList) {
-                        if (stampModel.getShop_id() == shopModel.get()) {
-                            stampModel.plusCount();
-                            stampModelList.add(stampModel);
-                            userModel.get().setStamps(stampModelList);
+                        if (Objects.equals(stampModel.getShop_id().getShop_id(), shopModel.get().getShop_id())) {
+                            StampModel stampModel1 = new StampModel(stampModel.getShop_id(),stampModel.getCount());
                             Integer stampLimit = shopModel.get().getStamp_limit();
-                            if (stampLimit <= stampModel.getCount() && (stampLimit % stampModel.getCount()) == 0) {
+                            if ((stampModel.getCount()+1)%stampLimit  == 0) {
+                                stampModel1.resetCount();
                                 CouponModel couponModel = new CouponModel(shopModel.get());
-                                List<CouponModel> couponModels = new ArrayList<>();
+                                List<CouponModel> couponModels = userModel.get().getCoupons();
                                 couponModels.add(couponModel);
                                 userModel.get().setCoupons(couponModels);
+                            } else {
+                                stampModel1.plusCount();
                             }
+                            stampModelList.replaceAll(stampModel2 -> stampModel2 == stampModel ? stampModel1 : stampModel2);
+                            userModel.get().setStamps(stampModelList);
                             userRepository.save(userModel.get());
                             log.info("addStamp : Success");
                             return "success";
